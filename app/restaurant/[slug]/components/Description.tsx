c53d5601c082a044efadd2adabb0a91c6da9a50f
Guilherme <gui.wustro@gmail.com>
2023-03-31 22:32:09 -0300

    feat: (home) create description component

---
+++ app/restaurant/[slug]/components/Description.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Description = () => {
+	return (
+		<div className="mt-4">
+			<p className="text-lg font-light">
+				Os clássicos que você ama preparados com um toque perfeito, todos
+				servidos em uma atmosfera perfeita. Essa é a promessa da Milestones. Por
+				isso, quer esteja a celebrar um marco, a aproveitar ao máximo a Happy
+				Hour ou a desfrutar de um brunch com os amigos, pode ter a certeza de
+				que cada experiência Milestones é simples e perfeitamente memorável.
+			</p>
+		</div>
+	);
+};
+
+export default Description;
