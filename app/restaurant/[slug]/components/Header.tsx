d197acc2f3803325c7e19316f3e2deccb5f0430a
Guilherme <gui.wustro@gmail.com>
2023-03-31 22:58:06 -0300

    feat: (restaurant) create header component

---
+++ app/restaurant/[slug]/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<div className="h-96 overflow-hidden">
+			<div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+				<h1 className="text-7xl text-white captitalize text-shadow text-center">
+					Restaurante da mamãe (São Paulo)
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
