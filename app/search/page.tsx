06308c48295ae3b1cb1b7a5a532bfdf53ff2959b
Guilherme <gui.wustro@gmail.com>
2023-04-03 23:33:28 -0300

    feat: (search) page

---
+++ app/search/page.tsx
@@ -0,0 +1,23 @@
+import NavBar from "../components/NavBar";
+import Header from "./components/Header";
+import SearchSideBar from "./components/SearchSideBar";
+import RestaurantCard from "./components/RestaurantCard";
+
+const Search = () => {
+	return (
+		<main className="bg-gray-100 min-h-screen w-screen">
+			<main className="max-w-screen-2xl m-auto bg-white">
+				<NavBar />
+				<Header />
+				<div className="flex py-4 m-auto w-2/3 justify-between items-start">
+					<SearchSideBar />
+					<div className="w-5/6">
+						<RestaurantCard />
+					</div>
+				</div>
+			</main>
+		</main>
+	);
+};
+
+export default Search;
